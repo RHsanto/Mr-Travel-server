467cbbac2eb9a9ba273ed151a346ed1d516a13c6
RHsanto <rakibulsanto789@gmail.com>
2022-05-02 02:19:11 +0600

    add initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const express = require('express');
+const cors = require('cors')
+const app =express();
+const port = process.env.PORT || 8000;
+
+//midalware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.send('Running the server on Mr. Travel');
+})
+app.listen(port, () => {
+  console.log('Running the server on Mr. Travel',port)
+})
